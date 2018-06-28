14684085b3618aee3a3da1c4fa5f6dd5a4a06ee0
vibecastor <vibecastor@gmail.com>
2018-06-27 17:59:57 -0700

    adding more base components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,14 +2,20 @@ import React from 'react';
 
 import Header from '../header/header';
 import Profile from '../../assets/mike.jpg';
+import Projects from '../projects/projects';
+import About from '../about/about';
+import Contact from '../contact/contact';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-    <Header/>
-        <h1>Full-stack Javascript Developer</h1>
-        <img src={Profile} alt="profile pic" height="25%" width="25%"></img>
+        <Header/>
+          <h1>Full-stack Javascript Developer</h1>
+          <img src={Profile} alt="profile pic" height="25%" width="25%"></img>
+        <About/>
+        <Projects/>
+        <Contact/>
       </div>
     );
   }
---
+++ src/components/projects/projects.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Projects extends React.Component {
+  render() {
+    return (
+      <h1>Projects</h1>
+    );
+  }
+}
