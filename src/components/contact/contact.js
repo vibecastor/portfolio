fd6f78ef245529eb7b705fdc73fce2c870e771de
vibecastor <vibecastor@gmail.com>
2018-07-03 13:22:15 -0700

    styling the contact component

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -6,7 +6,6 @@ export default class Contact extends React.Component {
   render() {
     return (
       <div className='contact'>
-      <h1>Contact</h1>
         <h2>Interested in contacting me regarding a project?</h2>
         <h3 className='email'><a href='mailto:vibecastor@gmail.com'>Email me!</a></h3>
       </div>
